78bc380613a5984077b64637c50a318821c53c8d
Dan Gutt <dgutt10@gmail.com>
2019-11-08 09:25:10 -0600

    Can start sessions with Neo4j with bolt adaptor instead of http adaptor

--- lib/imdb_parser/static_nodes.rb
+++ lib/imdb_parser/static_nodes.rb
@@ -1,10 +1,11 @@
 module ImdbParser
     class StaticNodes
         require 'neo4j/core/cypher_session/adaptors/bolt'
-        require 'neo4j/core/cypher_session/adaptors/http'
         def self.create_month_nodes
-            # http_adaptor = Neo4j::Core::CypherSession::Adaptors::HTTP.new('http://neo4j:123456@localhost:7474')
-            bolt_adaptor = Neo4j::Core::CypherSession::Adaptors::Bolt.new('bolt://neo4j:12345@localhost:7687')
+            cert_store = OpenSSL::X509::Store.new
+            path_to_certificate = "/Users/dangutt/Library/Application Support/Neo4j Desktop/Application/neo4jDatabases/database-7dce9f52-ab9a-4eb7-8353-c97e702fd774/installation-3.5.12/certificates/neo4j.cert"
+            cert_store.add_file(path_to_certificate)
+            bolt_adaptor = Neo4j::Core::CypherSession::Adaptors::Bolt.new('bolt://neo4j:123456@localhost:7687', ssl: {cert_store: cert_store})
 
             neo4j_session = Neo4j::Core::CypherSession.new(bolt_adaptor)
 
