c51aad1d53d5cb0ad081e66d22c84d395111fb1a
Dan Gutt <dgutt10@gmail.com>
2019-11-08 21:10:09 -0600

    Created Driver Class to hold code to create drivers and sessions. Also renamed StaticNodes Class to MonthNodes.

---
+++ lib/driver.rb
@@ -0,0 +1,17 @@
+require 'neo4j/core/cypher_session/adaptors/bolt'
+
+module Neo4j
+    class Driver
+        def initialize
+            cert_store = OpenSSL::X509::Store.new
+            path_to_certificate = ENV["NEO4J_CERT_PATH"]
+            cert_store.add_file(path_to_certificate)
+            bolt_path = 'bolt://neo4j:' + ENV["NEO4J_PASSWORD"] + '@localhost' + ENV["NEO4J_PORT"]
+            @bolt_adaptor = Neo4j::Core::CypherSession::Adaptors::Bolt.new(bolt_path, timeout: 10, ssl: {cert_store: cert_store})
+        end
+
+        def session
+            return Neo4j::Core::CypherSession.new(@bolt_adaptor)
+        end
+    end
+end
