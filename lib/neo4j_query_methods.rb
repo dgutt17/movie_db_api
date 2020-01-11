9642515394c539327ad37ec2429df6776c86b301
Dan Gutt <dgutt10@gmail.com>
2020-01-11 14:02:55 -0600

    removed old methods from Neo4jQueryMethods module

--- lib/neo4j_query_methods.rb
+++ lib/neo4j_query_methods.rb
@@ -12,13 +12,6 @@ module Neo4jQueryMethods
 
         unwind + ' ' + match_node_one + ' ' + match_node_two + ' ' + ' '+ create_relationship + increment
     end
-    # def batch_create_genre_relationships
-    #     "UNWIND {list} as row MATCH (from:Movie {imdb_id: row.from}) MATCH (to:Genre {name: row.to}) CREATE (from)-[rel:CATEGORIZED_AS]->(to) SET rel += row"
-    # end
-
-    # def batch_create_year_relationships
-    #     "UNWIND {list} as row MATCH (from:Movie {imdb_id: row.from}) MATCH (to:Year {value: row.to}) CREATE (from)-[rel:RELEASED]->(to) SET rel += row"
-    # end
 
     def parse_node_label(label)
         label.titleize.split(' ').join('_')
