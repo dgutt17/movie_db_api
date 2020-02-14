d49b98c687598891ad3fb04e6341ea6f3f343ee8
Dan Gutt <dgutt10@gmail.com>
2020-02-13 21:38:12 -0600

    Renamed methods to import from create

--- lib/imdb_importer.rb
+++ lib/imdb_importer.rb
@@ -7,7 +7,7 @@ class ImdbImporter
     end
 
     def batch_create
-        create_static_nodes
+        import_static_nodes
         import_genres
         importing_content_nodes_and_relationships
         importing_principal_nodes_and_known_for_relationships
@@ -16,13 +16,13 @@ class ImdbImporter
 
     private
 
-    def create_static_nodes
+    def import_static_nodes
         puts "Creating Static Nodes................................."
-        create_indices
-        create_years
-        create_months
-        create_days
-        create_imdb_scores
+        import_indices
+        import_years
+        import_months
+        import_days
+        import_imdb_scores
     end
 
     def importing_content_nodes_and_relationships
@@ -41,23 +41,23 @@ class ImdbImporter
         TitlePrincipalsImporter.new(@content_hash).run
     end
 
-    def create_indices
+    def import_indices
         Index.create
     end
 
-    def create_years
+    def import_years
         Year.create
     end
 
-    def create_months
+    def import_months
         Month.create
     end
 
-    def create_days
+    def import_days
         Day.create
     end
 
-    def create_imdb_scores
+    def import_imdb_scores
         ImdbScore.create
     end
 
