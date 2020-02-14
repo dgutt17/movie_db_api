13c391359a8f175e8fd0fe683fe5bb7f758d59dd
Dan Gutt <dgutt10@gmail.com>
2020-02-13 21:36:22 -0600

    Added some methods in the imdb_importer and created the title_principals_importer

--- lib/imdb_importer.rb
+++ lib/imdb_importer.rb
@@ -7,30 +7,63 @@ class ImdbImporter
     end
 
     def batch_create
-        phase_one
-        phase_two
-        phase_three
+        create_static_nodes
+        import_genres
+        importing_content_nodes_and_relationships
+        importing_principal_nodes_and_known_for_relationships
+        importing_principal_content_relationships
     end
 
     private
 
-    def phase_one
-        puts "Phase One................................."
-        Index.create
-        Year.create
-        Month.create
-        Day.create
-        ImdbScore.create
+    def create_static_nodes
+        puts "Creating Static Nodes................................."
+        create_indices
+        create_years
+        create_months
+        create_days
+        create_imdb_scores
     end
 
-    def phase_two
-        puts "Phase Two................................."
+    def importing_content_nodes_and_relationships
+        puts "Importing Content Nodes and their associated relationships................................."
         Genre.create
         @content_hash = TitleBasicsImporter.new.run
     end
 
-    def phase_three
-        puts "Phase Three................................."
+    def importing_principal_nodes_and_known_for_relationships
+        puts "Importing Principal Nodes and Known For Relationships................................."
         PrincipalsImporter.new(@content_hash).run
     end
+    
+    def importing_content_to_principal_relationships
+        puts 'Importing content to principal relationships...........................................'
+        TitlePrincipalsImporter.new(@content_hash).run
+    end
+
+    def create_indices
+        Index.create
+    end
+
+    def create_years
+        Year.create
+    end
+
+    def create_months
+        Month.create
+    end
+
+    def create_days
+        Day.create
+    end
+
+    def create_imdb_scores
+        ImdbScore.create
+    end
+
+    def import_genres
+        puts 'Importing Genre Nodes....................................'
+        Genre.create
+    end
+    
 end
