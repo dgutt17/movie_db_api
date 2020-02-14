f3fb6c2dff7c57c597eaa32b3d79f984281636f9
Dan Gutt <dgutt10@gmail.com>
2020-02-13 21:13:57 -0600

    Created batch_merge and batch_create methods for nodes and relationships

--- lib/query_methods.rb
+++ lib/query_methods.rb
@@ -1,10 +1,23 @@
 module Neo4j
     module QueryMethods
         def batch_create_nodes(label)
-            "UNWIND {list} as row MERGE (n:#{parse_node_label(label)} {imdb_id: row.id}) ON CREATE SET n += row.properties return n"
+            "UNWIND {list} as row CREATE (n:#{parse_node_label(label)} {imdb_id: row.id}) SET n += row.properties return n"
         end
 
         def batch_create_relationships(args)
+            unwind = 'UNWIND {list} as row'
+            match_node_one = "MATCH (from:#{parse_node_label(args[:node_label_one])} #{args[:match_obj_one]})"
+            match_node_two = "MATCH (to:#{parse_node_label(args[:node_label_two])} #{args[:match_obj_two]})"
+            create_relationship = "CREATE (from)-[rel:#{parse_rel_label(args[:rel_label])}]->(to)"
+
+            unwind + ' ' + match_node_one + ' ' + match_node_two + ' ' + ' '+ create_relationship
+        end
+
+        def batch_merge_nodes(label)
+            "UNWIND {list} as row MERGE (n:#{parse_node_label(label)} {imdb_id: row.id}) ON CREATE SET n += row.properties return n"
+        end
+
+        def batch_merge_relationships(args)
             unwind = 'UNWIND {list} as row'
             match_node_one = "MATCH (from:#{parse_node_label(args[:node_label_one])} #{args[:match_obj_one]})"
             match_node_two = "MATCH (to:#{parse_node_label(args[:node_label_two])} #{args[:match_obj_two]})"
