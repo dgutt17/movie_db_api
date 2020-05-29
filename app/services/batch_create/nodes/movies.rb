4efad0bc908cc287278f92fd4bcedfba3d071f94
Dan Gutt <dgutt10@gmail.com>
2020-05-29 09:15:25 -0500

    Trying to remove extra code with better architecture

--- app/services/batch_create/nodes/movies.rb
+++ app/services/batch_create/nodes/movies.rb
@@ -1,34 +1,37 @@
 # require 'query_methods'
 
 module BatchCreate
-  module Nodes
-    class Movies
-      include Neo4j::QueryMethods
-      include ImporterParsingMethods
+  class Nodes
+    include Neo4j::QueryMethods
+    include ImporterParsingMethods
 
-      attr_reader :nodes
+    attr_reader :nodes, :type
 
-      def initialize
-        @nodes = []
-      end
+    def initialize(type:)
+      @nodes = []
+      @type = type
+    end
 
-      def collect(args)
-        nodes << Movie.new(args).node if parse_type(args[:titleType]) == :movie
-        puts "Created movie: #{args[:tconst]}"
-      end
+    def collect(args)
+      nodes << klass.new(args).node if parse_type(args[:titleType]) == type
+      puts "Created movie: #{args[:tconst]}"
+    end
 
-      def import
-        cypher_obj = $neo4j_session.query(batch_create_nodes(labels), list: nodes)
-        @nodes = []
+    def import
+      cypher_obj = $neo4j_session.query(batch_create_nodes(labels), list: nodes)
+      @nodes = []
 
-        cypher_obj
-      end
+      cypher_obj
+    end
 
-      private
+    private
 
-      def labels
-        "#{Labels::CONTENT}:#{Labels::MOVIE}"
-      end
+    def labels
+      "#{Labels::CONTENT}:#{Labels::MOVIE}"
+    end
+
+    def klass
+      @klass ||= type.to_s.split('_').map{|word| word.capitalize}.join('').constantize
     end
   end
 end
